fc1f74cdc5531ce6614aacd83c474078c44560cc
Ibrahim <33503887+ibrahimendire@users.noreply.github.com>
2020-10-24 11:42:03 -0500

    Add files via upload

---
+++ Assigment_2/TechJobsOO/CoreCompetency.cs
@@ -0,0 +1,40 @@
+﻿using System;
+namespace TechJobsOO
+{
+    public class CoreCompetency
+    {
+        public int id { get;}
+        private static int nextId = 1;
+        public string value { get; set;}
+
+        // TODO: Change the fields to auto-implemented properties.
+
+        public CoreCompetency()
+        {
+            id = nextId;
+            nextId++;
+        }
+
+        public CoreCompetency(string v) : this()
+        {
+            value = v;
+        }
+
+        public override bool Equals(object obj)
+        {
+            return obj is CoreCompetency competency &&
+                   id == competency.id;
+        }
+
+        public override int GetHashCode()
+        {
+            return HashCode.Combine(id);
+        }
+
+        public override string ToString()
+        {
+            return value;
+        }
+    }
+    }
+
---
+++ Assigment_2/TechJobsOO/Job.cs
@@ -0,0 +1,60 @@
+﻿using System;
+namespace TechJobsOO
+{
+    public class Job
+    {
+        public int Id { get; }
+        private static int nextId = 1;
+
+        public string Name { get; set; }
+        public Employer EmployerName { get; set; }
+        public Location EmployerLocation { get; set; }
+        public PositionType JobType { get; set; }
+        public CoreCompetency JobCoreCompetency { get; set; }
+
+        // TODO: Add the two necessary constructors.
+        public Job()
+        {
+            Id = nextId;
+            nextId++;
+        }
+
+        public Job(string name, Employer employerName, Location employerLocation, PositionType jobType, CoreCompetency jobCoreCompetency) : this()
+        {
+            Name = name;
+            EmployerName = employerName;
+            EmployerLocation = employerLocation;
+            JobType = jobType;
+            JobCoreCompetency = jobCoreCompetency;
+        }
+
+        // TODO: Generate Equals() and GetHashCode() methods.
+
+
+        public override bool Equals(object obj)
+        {
+            return obj is Job job &&
+                   Id == job.Id;
+                 
+        }
+       
+
+        public override int GetHashCode()
+        {
+            return HashCode.Combine(Id, Name);
+        }
+        public override string ToString()
+        {
+            string notAvailable = " “Data not available” ";
+            string jobToString = "";//will update below in if/else
+            if (Name == null || Name.Trim() == "")
+            {
+                return notAvailable;
+            }
+            else
+            {
+                return jobToString;
+            }
+        }
+    }
+}
